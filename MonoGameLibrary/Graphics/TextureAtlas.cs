dccd937f276d0e7fdb862e466df71b585f43bfe2
Guillaume B <benard.guillaume0@gmail.com>
2025-09-17 23:48:56 +0200

    Chapter 08: The Sprite Class
    Explore creating a reusable Sprite class to efficiently manage sprites and their rendering properties, including position, rotation, scale, and more.

--- MonoGameLibrary/Graphics/TextureAtlas.cs
+++ MonoGameLibrary/Graphics/TextureAtlas.cs
@@ -10,6 +10,18 @@ namespace MonoGameLibrary.Graphics;
 
 public class TextureAtlas
 {
+
+    /// <summary>
+    /// Creates a new sprite using the region from this texture atlas with the specified name.
+    /// </summary>
+    /// <param name="regionName">The name of the region to create the sprite with.</param>
+    /// <returns>A new Sprite using the texture region with the specified name.</returns>
+    public Sprite CreateSprite(string regionName)
+    {
+        TextureRegion region = GetRegion(regionName);
+        return new Sprite(region);
+    }
+
     private Dictionary<string, TextureRegion> _regions;
 
     /// <summary>
